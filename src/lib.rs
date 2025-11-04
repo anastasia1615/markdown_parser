1d1f9cc46818d1c08fc08dd2ea681ab5f0e3c784
anastasia1615 <a.pokormiak@ukma.edu.ua>
2025-11-04 22:41:42 +0200

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,3 @@ use pest_derive::Parser;
 #[grammar = "grammar.pest"]
 pub struct MarkdownParser;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use pest::Parser;
-
-    #[test]
-    fn unit_header() {
-        let input = "# testing some shi";
-        let result = MarkdownParser::parse(Rule::header1, input);
-        assert!(result.is_ok());
-    }
-}
