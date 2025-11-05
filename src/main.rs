fc2f1ef6fbfbdfd364bdba4493eb3fd730214b1f
anastasia1615 <a.pokormiak@ukma.edu.ua>
2025-11-05 23:51:05 +0200

    added a test in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,12 @@ fn main() -> anyhow::Result<()> {
     println!("{:#?}", parsed);
     Ok(())
 }
+#[test]
+fn header1() {
+    let input = "# oh please please please let me get what i want";
+    let result = MarkdownParser::parse(Rule::header1, input);
+    assert!(result.is_ok());
+
+    let pair = result.unwrap().next().unwrap();
+    assert_eq!(pair.as_str(), input);
+}
