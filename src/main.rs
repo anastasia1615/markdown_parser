75511d4304aa754d1c945fad859b16e04d8b7724
anastasia1615 <a.pokormiak@ukma.edu.ua>
2025-11-07 17:11:37 +0200

    added 2 rules to grammar + tests (unit + int) (to be fixed soon)

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use pest::Parser;
 use markdown_parser::*;
 
 fn main() -> anyhow::Result<()> {
-    let input = "# I love photography";
-    let parsed = MarkdownParser::parse(Rule::header1, input)?;
-    println!("{:#?}", parsed);
+    let input1 = "# I love photography";
+    let parsed1 = MarkdownParser::parse(Rule::header1, input1)?;
+    println!("{:#?}", parsed1);
+    let input2 = "## idk what to write";
+    let parsed2 = MarkdownParser::parse(Rule::header2, input2)?;
+    println!("{:#?}", parsed2);
+    let input3 = "### i ran out of ideas sorry";
+    let parsed3 = MarkdownParser::parse(Rule::header3, input3)?;
+    println!("{:#?}", parsed3);
     Ok(())
 }
 #[test]
@@ -12,7 +18,22 @@ fn header1() {
     let input = "# oh please please please let me get what i want";
     let result = MarkdownParser::parse(Rule::header1, input);
     assert!(result.is_ok());
-
+    let pair = result.unwrap().next().unwrap();
+    assert_eq!(pair.as_str(), input);
+}
+#[test]
+fn header2() {
+    let input = "## Be quiet and drive (far away)";
+    let result = MarkdownParser::parse(Rule::header2, input);
+    assert!(result.is_ok());
+    let pair = result.unwrap().next().unwrap();
+    assert_eq!(pair.as_str(), input);
+}
+#[test]
+fn header3() {
+    let input = "### My mind is a mountain";
+    let result = MarkdownParser::parse(Rule::header3, input);
+    assert!(result.is_ok());
     let pair = result.unwrap().next().unwrap();
     assert_eq!(pair.as_str(), input);
 }
--- tests/gramtests.rs
+++ tests/gramtests.rs
@@ -5,14 +5,33 @@ use markdown_parser::*;
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::anyhow;
 
     #[test]
-    fn int_header() -> anyhow::Result<()> {
+    fn int_header1() -> anyhow::Result<()> {
         let mut got = MarkdownParser::parse(Rule::header1, "# I love beer")?;
         let got = got.next().ok_or_else(|| anyhow!("No pairs"))?;
         assert_eq!(got.as_str(), "# I love beer");
         assert!(MarkdownParser::parse(Rule::header1, "# ").is_err());
         assert!(MarkdownParser::parse(Rule::header1, "I").is_err());
+        Ok(())
+    }
+    #[test]
+    fn int_header2() -> anyhow::Result<()> {
+        let mut got = MarkdownParser::parse(Rule::header2, "## I don't know what to write")?;
+        let got = got.next().ok_or_else(|| anyhow!("No pairs"))?;
+        assert_eq!(got.as_str(), "## I don't know what to write");
+        assert!(MarkdownParser::parse(Rule::header2, "## ").is_err());
+        assert!(MarkdownParser::parse(Rule::header2, "# Rawr").is_err());
+        Ok(())
+    }
+    #[test]
+    fn int_header3() -> anyhow::Result<()> {
+        let mut got = MarkdownParser::parse(Rule::header3, "### Lalalalala")?;
+        let got = got.next().ok_or_else(|| anyhow!("No pairs"))?;
+        assert_eq!(got.as_str(), "### Lalalalala");
+        assert!(MarkdownParser::parse(Rule::header3, "### ").is_err());
+        assert!(MarkdownParser::parse(Rule::header3, "## Yo").is_err());
         Ok(())
     }
 }
